8aba2d3ba03bcebd73e1f97a1f8802e04d40a822
Adi <asriram3@illinois.edu>
2020-11-18 13:46:02 -0600

    added friend list

---
+++ app/src/main/java/com/example/word/new_challenge/NewChallengeActivity.java
@@ -0,0 +1,18 @@
+package com.example.word.new_challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.word.R;
+
+public class NewChallengeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_challenge);
+
+
+    }
+}
--- app/src/main/java/com/example/word/ui/newchallenge/NewChallengeFragment.java
+++ app/src/main/java/com/example/word/ui/newchallenge/NewChallengeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,8 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.word.R;
 
+import java.util.List;
+
 public class NewChallengeFragment extends Fragment {
 
     private NewChallengeViewModel newChallengeViewModel;
@@ -23,11 +26,12 @@ public class NewChallengeFragment extends Fragment {
         newChallengeViewModel =
                 new ViewModelProvider(this).get(NewChallengeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_new_challenge, container, false);
-        final TextView textView = root.findViewById(R.id.text_new_challenge);
+//        final TextView textView = root.findViewById(R.id.text_new_challenge);
+//        ListView listView = root.findViewById(R.id.new_challenge_friend_list);
         newChallengeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+//                textView.setText(s);
             }
         });
         return root;
